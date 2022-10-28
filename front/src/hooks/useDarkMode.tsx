054bb382aa9e4b90db44abc5f9d478175b917ac4
Nico <nicolasvergne88@gmail.com>
2022-10-28 22:51:39 +0100

    add hooks for localstorage and darkmode

---
+++ front/src/hooks/useDarkMode.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import useLocalStorage from '@hooks/useLocalStorage';
+
+const useDarkMode = () => {
+  const [enabled, setEnabled] = useLocalStorage('dark-theme');
+  const isEnabled = enabled;
+
+  useEffect(() => {
+    const className = 'dark';
+    const bodyClass = window.document.body.classList;
+
+    isEnabled ? bodyClass.add(className) : bodyClass.remove(className);
+  }, [enabled, isEnabled]);
+
+  return [enabled, setEnabled];
+};
+
+export default useDarkMode;
