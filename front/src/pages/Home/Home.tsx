aef56bf01e4d356f3f3055c4b6e3183d85d59909
Nico <nicolasvergne88@gmail.com>
2022-10-29 01:50:34 +0100

    add router and pages template

---
+++ front/src/pages/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+import './Home.scss';
+
+import Sidebar from '@components/Sidebar/Sidebar';
+
+const Home:FC = () => {
+
+
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default Home;
